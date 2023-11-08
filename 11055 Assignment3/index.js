f41f3017a49204e038d32c586765e058e99460e0
chrischrisxx <104652629+chrischrisxx@users.noreply.github.com>
2023-11-08 18:39:16 +0800

    Add files via upload

---
+++ 11055 Assignment3/index.js
@@ -0,0 +1,120 @@
+const data = [
+  {
+    menuText: 'BUSHFIRE BASICS', // 导航名称
+    image: 'LightningexJGittins.jpg', // 对应asset中的图片文件
+    content: [ // 显示的内容，可以无限增加
+      'Bushfire Basics',
+      'It is not possible to completely eradicate bushfires. Even if there were no accidents, and no acts of human carelessness or incendiarism, bushfires would still be started by lightning, as they were for many millennia before the arrival of humans on the Australian continent. However, we can minimize the number of human-caused fire ignitions by public awareness programs, and much effort has been devoted to this over many years. Despite this effort, we still have large numbers of arson ignitions each fire season. ',
+      'Sadly, the number of incidents of arson has increased significantly in recent years, which would indicate that there is a limit to the value of education programs. It is possible that a dramatic increase in the penalties for arson might reduce its incidence, but it is most unlikely to eliminate it. Arsonists are frequently not rational and do not weigh the possible consequences of their actions against likely penalties. Furthermore, in the current international political climate, we have to consider the possibility that terrorists might use arson as part of a disruptive program.',
+      'If we accept that there will still be a serious number of bushfire ignitions each fire season, then the next best thing we can do is to minimize their impact by suppressing them while they are small or managing bushland fuel in a strategic way to minimize the intensity of fires that do occur.'
+    ]
+  },
+  {
+    menuText: 'SMOKE',
+    image: 'vic3.jpg',
+    content: [
+      'The Smoke Issue',
+      'The smoke that arises from bushfires, whether it comes from prescribed burns or from wildfires, is a controversial subject in Western Australia. Every time the metropolitan area is affected by smoke drift from a prescribed burn in nearby forests there is an outcry in the media. Paradoxically, there is little comment on smoke from a wildfire, even though it is much thicker and often long-lasting: it seems to be accepted that this will happen.',
+      'To some, smoke is simply a matter of air pollution, and therefore should be stopped. To others, it is an inconvenience, especially if they suffer from respiratory disease. In the latter case it is possible that smoke particulates can cause increased difficulty with breathing. However, there are well-known steps sufferers can take to alleviate the breathing difficulty.',
+      'However, bushfire smoke in the Australian environment is not air pollution. Smoke is a natural, and necessary, part of our environment. Native vegetation has evolved in the presence of fire and the landscapes we see today have been moulded by fire. Some plants actually depend on periodic fire for reproduction, and almost all species have some sort of adaptation that enables them to tolerate our fire-friendly climate. Many native plants are also fire-friendly and contain essential oils that are highly inflammable. This feature increases the intensity of a fire and makes forest fires in native vegetation Australia faster-spreading than similar fires in other countries.'
+    ]
+  },
+  {
+    menuText: 'HISTORY',
+    image: 'feb2-1.jpeg',
+    content: [
+      'History',
+      'Many Perth residents would remember the afternoon of Saturday 15 January 2005 when smoke and ash from a bushfire burning in Perth’s hills blanketed much of the metropolitan area. The fire was started by an arsonist who lit separate fires in dense bush, which were fuelled by strong easterly winds. By the time crews arrived, the fire was burning through the tree crowns, causing total defoliation, and embers were spotting well ahead of the headfire. Under these conditions, attack on the head fire was too dangerous and aerial suppression was ineffective. On 18 January lightning struck to the east of the main fire starting another one.  ',
+      'Lessons learnt from bushfires in Sydney and Canberra indicated the potential for this bushfire to affect the quality of Perth’s water supply. The potential impacts were identified as short- term increases in surface run off and yield; increased water cloudiness and colour; increased organic material leading to a decrease in oxygen; release of nutrients; and altered surface soil properties. In the Mundaring Catchment, areas adjacent to the reservoir and a large area of steep land in the Darkin River sub-catchment were burnt by very high-intensity bushfire.',
+      'As Mundaring is one of the main water sources to the Goldfields and agricultural region (and at that time water treatment was chlorination and fluoridation), minimising the impacts was important and land and water managers successfully implemented a range of remedial actions before winter rains to minimise any possible influence on water quality. The Water Corporation also funded data collection and research.',
+      'Data showed that waterflow doubled but also that 500 tonnes of soil were deposited in the streambed of the Little Darkin River, only one of many streams that feed into Mundaring Weir that were affected by the bushfire. Ash and silt can disrupt the effectiveness of chlorination treatment, which is essential for maintaining water quality. However, the turbidity plume was managed through in-stream dosing and so the water quality of Mundaring was unaffected. As a result, there was no influence on the treatment process or provision of safe drinking quality water to the Goldfields and agricultural water supply. Data also showed that terrestrial and aquatic biodiversity was significantly impaired and between 1.5 and 2.3 million trees were killed, many of which were large fauna habitat trees.'
+    ]
+  },
+  {
+    menuText: 'IMPACT',
+    image: 'Canberraburnt21203.jpg',
+    content: [
+      'Impacts of Bushfires',
+      'Here we are considering the adverse effects of high intensity wildfires on the community and the environment. It should be remembered that it is not just intensity of a bushfire that matters. The extent of a bushfire is also very important. A large bushfire can cause multiple direct impacts: on life and property, on the survival of fauna populations, on water resources, and indirectly on government budgets and insurance costs. A large bushfire will also generate huge amounts of smoke.',
+      'In severe fire seasons the damage caused by bushfires is astronomical. In the 2003 Canberra fire disaster, for example, the cost to insurers alone was $257 million (The West Australian, 13 Feb, 2003). The total cost of the fires, including the cost of suppression measures and lost productivity, would be much greater than this figure. Add to it the loss of life, personal trauma, loss of invaluable personal possessions and destruction of scientific equipment and data from the Mt Stromlo Observatory, and the ramifications of such disasters start to become apparent. ',
+      'Of course, the forests incinerated by these intense bushfires also suffer great damage. Unlike the usual description employed in the media, most native forests are not “destroyed” by wildfire. Some are. Ash-type eucalypt forests are killed in this way, as the 1939 Victorian fire disaster clearly demonstrated. Drier forest types, such as jarrah in WA are not destroyed, although they are severely damaged and many trees are killed by high intensity bushfires. ',
+    ]
+  },
+  {
+    menuText: 'CONSEQUENCE',
+    image: 'Consequence.jpeg',
+    content: [
+      'Consequence',
+      'The south-west of WA has experienced below-average rainfall dating back to the 1970s. There have been dry periods in WA before, e.g., in the late 1800s. The current below-average rainfall period could be a permanent change, or could be a long-term cyclical event. Nobody knows.',
+      'The reason for below-average rainfall is that the low pressure systems that bring the cold fronts and winter rains are now passing to the south of the State. The reasons for this are not known or are disputed. However, the cause is irrelevant, because there is nothing we can do about it. We are concerned with the consequence, how this effects bushfire behaviour and with the adoption of remedial actions that are practical and economical.',
+      'The critical consequence is that below-average rainfall leads to drier fuels especially earlier in the fire season. Drier fuels lead, in turn, to more intense fire behaviour, especially during the period in which fuel reduction burning has traditionally occurred. Thus, drier fuels can result in more intense fires, and reduce the number of suitable days for fuel reduction burning. An indirect consequence is the need to employ personnel on fire-fighting instead of fuel reduction burning during the spring period. ',
+      'There is a significant related factor: over the drier period, there has also been a decline in fuel reduction burning, especially in south-west forests. This produced a “double whammy”: fuels are both heavier and drier.',
+      ' Compared to slope, wind strength, fuel quantity and dryness, temperature is an insignificant driver of fire behaviour. Experienced firefighters do not fear a 40-degree day per se. This is because even on a hot day, a fire in one or two-year old fuel can be controlled; on the same day a fire in 20-year old fuels with high winds would usually be unstoppable.',
+      'There is nothing that anyone in WA can do right now to return rainfall to the levels that prevailed in the 1950s and 1960s. Even if the current drought is caused by CO2 emissions, this is a world-wide phenomenon, and even if effective emission-reduction programs are introduced worldwide, they will not take effect for many years. In any case the relationship between atmospheric CO2 concentrations and rainfall in the south-west of WA is unknown.',
+    ]
+  },
+  {
+    menuText: 'WHAT YOU NEED TO KNOW',
+    image: 'p6.jpeg',
+    content: [
+      'What You Need To Know',
+      'Bushfires generate overwhelming showers of burning embers and pieces of burning bark. Burning pieces of bark can be carried several kilometres ahead of the main fire front and start “spot fires” in unexpected places. For this reason it is important that all parts of the landscape are maintained in a low fuel condition. Odd patches of bushland scattered among housing developments are the sort of places that will easily catch fire from these embers and quickly generate even more embers. This does not mean a “scorched earth” approach everywhere, as a fire in fuel that is less than 4 years old can be easily and safely controlled under most conditions.',
+      'The essential thing is to prepare your property well in advance of the fire season. You need to be bushfire-ready by the end of October in Western Australia. All leaf litter needs to be cleared up and inflammable shrubs around the house to a distance of at least 20 metres, and maintained that way until April the next year. Trees continue to shed leaves all through the summer, especially eucalypts, and they keep lodging in gutters. The gutters need to be cleaned out every month or so if there are trees close to the house. '
+    ]
+  }
+]
+
+const menuList = document.querySelector('.menu-list')
+const mainCnntent = document.getElementById('main-content')
+let html = [`<div class="menu-item" onclick="handleClick(0)">HOME</div>`]
+let cHtml = [`<div class="swiper-slide first">
+<div class="swiper-container-2">
+  <div class="swiper-wrapper">
+    <div class="swiper-slide" style="background-image: url(./asset/b1.webp);"></div>
+    <div class="swiper-slide" style="background-image: url(./asset/b2.jpg);"></div>
+    <div class="swiper-slide" style="background-image: url(./asset/b3.webp);"></div>
+  </div>
+  <div class="swiper-button-next"></div>
+  <div class="swiper-button-prev"></div>
+</div>
+</div>`]
+for (let i = 0; i < data.length; i++) {
+  let item = data[i]
+  html.push(`<div class="menu-item" onclick="handleClick(${i + 1})">${item.menuText}</div>`)
+  cHtml.push(`<div class="swiper-slide">
+    <div class="inner-content">
+      <img src="./asset/${item.image}" />
+      <div>
+        ${item.content.map((elem, index) => {
+          if (index == 0) {
+            return `<h3 style="text-align: left; margin-bottom: 16px; color: rgb(25, 255, 25)">${elem}</h3>`
+          } else {
+            return `<p class="text">${elem}</p>`
+          }
+        }).join('')}
+      </div>
+    </div>
+  </div>`)
+}
+
+menuList.innerHTML = html.join('')
+mainCnntent.innerHTML = cHtml.join('')
+
+function handleClick(index) {
+  swiper.slideTo(index)
+}
+
+var swiper = new Swiper('.swiper-container', {
+  direction: 'vertical',
+  mousewheel: true,
+  speed: 1200,
+  allowTouchMove: false,
+  parallax: true,
+});
+
+var swiper2 = new Swiper('.swiper-container-2', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
---
+++ 11055 Assignment3/readme.md
@@ -0,0 +1,18 @@
+11055 Assignment(ONE PAGE SITE)
+
+Topic:
+Bushfires
+
+Titles:
+Bushfire Basics
+The Smoke Issue
+History
+Impacts of Bushfires
+Consequence
+What You Need To Know
+
+
+Reference:
+https://www.youtube.com/watch?v=LENuACsejws
+https://en.wikipedia.org/wiki/Bushfires_in_Australia
+https://www.bbc.com/news/world-australia-66793592
